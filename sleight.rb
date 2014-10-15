4e80e3f97e525c3c488c92602f33d6e1a12d9192
Brian Koser <brianmkoser@gmail.com>
2014-10-14 21:42:43 -0500

    Add create_recipe_list

--- sleight.rb
+++ sleight.rb
@@ -9,6 +9,45 @@ def create_html(jsonFile, destination, htmlFileName)
     File.open(File.join(destination, htmlFileName), 'w') {|f| f.write(Mustache.render(data)) }
 end
 
+def create_recipe_list(file_template_dir, recipes)
+    Mustache.template_file = file_template_dir
+    
+    list_template_found = false
+    list_template = ''
+    
+    file_template = IO.readlines(file_template_dir)
+    file_template.each do |line|
+        if line.include? '{{/recipes}}'
+            list_template_found = false
+            line.gsub!(/.*/, '')
+
+            list_template.strip!
+            recipe_links = ''
+            Dir.foreach(recipes) do |item|
+                next if item == '.' or item == '..'
+                json_file = JSON.parse(File.read(File.join(recipes, item)))
+                recipe = list_template.gsub '{{name}}', json_file['name']
+                # recipe.gsub! '{{url}}', json_file['url']
+                recipe_links = recipe_links + recipe + "\n"
+            end
+            
+            line.insert 0, recipe_links
+        end
+        
+        if list_template_found
+            list_template = list_template + line + "\n"
+            line.gsub!(/.*/, '')
+        end
+    
+        if line.include? '{{#recipes}}'
+            list_template_found = true
+            line.gsub!(/.*/, '')
+        end
+    end
+    
+    puts file_template
+end
+
 def create_recipes(template, src, dest)
     Mustache.template_file = template
     
@@ -49,6 +88,8 @@ img_dir_dest          = File.join output_dir, 'img'
 recipes_dir_dest      = File.join output_dir, 'recipes'
 recipes_template_path = File.join site_dir, '_templates', 'recipes', '_default.mustache'
 recipes_json_path     = File.join site_dir, '_data', 'recipes'
+recipe_list_src       = File.join site_dir, '_templates', 'recipes', 'index.mustache'
+recipe_list_dest      = File.join recipes_dir_dest, 'index.html'
 
 # Remove existing output so we can create the site from scratch
 FileUtils.rm_r output_dir if File.exists?(output_dir)
@@ -63,3 +104,7 @@ FileUtils.cp_r img_dir_src, img_dir_dest
 
 # Move recipes to output
 create_recipes(recipes_template_path, recipes_json_path, recipes_dir_dest)
+
+# Create recipe list
+FileUtils.cp_r recipe_list_src, recipe_list_dest
+create_recipe_list(recipe_list_dest, recipes_json_path)
